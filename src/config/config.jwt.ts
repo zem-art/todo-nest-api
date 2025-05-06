1cb93fc402030f7a578cb5d4277b70c208b982fb
zaid <amonsefra13@gmail.com>
2025-05-07 01:10:36 +0700

    update config jwt

--- src/config/config.jwt.ts
+++ src/config/config.jwt.ts
@@ -1,15 +1,12 @@
 import { registerAs } from "@nestjs/config";
 
-export default registerAs('jwt', () => ({
-    secret : process.env.JWT_SECRET_KEY || 'Hf7BkzJDWQUZRhPKFs9VxqG2LcYr6paAwnm8SEuy3tNbjMdgTe',
-    expiresIn: process.env.JWT_EXPIRES_IN || '10m',
-    issuer: process.env.JWT_ISSUER || 'boba.com',
-}))
+const defaultJWT = "Hf7BkzJDWQUZRhPKFs9VxqG2LcYr6paAwnm8SEuy3tNbjMdgTe"
 
-// export default () => ({
-//     jwt: {
-//         secret : process.env.JWT_SECRET_KEY || 'Hf7BkzJDWQUZRhPKFs9VxqG2LcYr6paAwnm8SEuy3tNbjMdgTe',
-//         expiresIn: process.env.JWT_EXPIRES_IN || '10m',
-//         issuer: process.env.JWT_ISSUER || 'boba.com',
-//     }
-// })
+export default registerAs(
+    'jwt', () => ({
+        secret_dev : process.env.JWT_SECRET_KEY_DEVELOPMENT || defaultJWT,
+        secret_prod : process.env.JWT_SECRET_KEY_PRODUCTION || defaultJWT,
+        expiresIn: process.env.JWT_EXPIRES_IN || '10m',
+        issuer: process.env.JWT_ISSUER || 'boba.com',
+    })
+)
